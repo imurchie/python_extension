5284e6f82a496410287012ac21d00b0c27a2bb2d
Isaac Murchie <isaac@saucelabs.com>
2014-02-22 12:18:04 -0800

    Pass structs back and forth

--- src/spammodule.c
+++ src/spammodule.c
@@ -1,20 +1,64 @@
 #include <Python.h>
+#include <math.h>
 
-static PyObject *
-spam_system(PyObject *self, PyObject *args)
-{
-    const char *command;
-    int sts;
+struct Point {
+    double x, y;
+};
 
-    if (!PyArg_ParseTuple(args, "s", &command))
-        return NULL;
-    sts = system(command);
-    return Py_BuildValue("i", sts);
+
+double distance(struct Point *p1, struct Point *p2) {
+  return hypot(p1->x - p2->x, p1->y - p2->y);
 }
 
+static void del_point(PyObject *obj) {
+  free(PyCapsule_GetPointer(obj, "Point"));
+}
+
+static struct Point *PyPoint_AsPoint(PyObject *obj) {
+  return (struct Point *) PyCapsule_GetPointer(obj, "Point");
+}
+
+static PyObject *PyPoint_FromPoint(struct Point *p, int must_free) {
+  return PyCapsule_New(p, "Point", must_free ? del_point : NULL);
+}
+
+static PyObject *py_Point(PyObject *self, PyObject *args) {
+  struct Point *p;
+  double x, y;
+  if(!PyArg_ParseTuple(args, "dd", &x, &y)) {
+    return NULL;
+  }
+
+  p = (struct Point *) malloc(sizeof(struct Point));
+  p->x = x;
+  p->y = y;
+
+  return PyPoint_FromPoint(p, 1);
+}
+
+static PyObject *py_distance(PyObject *self, PyObject *args) {
+  struct Point *p1, *p2;
+  PyObject *py_p1, *py_p2;
+  double result;
+
+  if(!PyArg_ParseTuple(args, "OO", &py_p1, &py_p2)) {
+    return NULL;
+  }
+  if(!(p1 = PyPoint_AsPoint(py_p1))) {
+    return NULL;
+  }
+  if(!(p2 = PyPoint_AsPoint(py_p2))) {
+    return NULL;
+  }
+
+  result = distance(p1, p2);
+  return Py_BuildValue("d", result);
+}
+
+
 static PyMethodDef SpamMethods[] = {
-    {"system",  spam_system, METH_VARARGS,
-     "Execute a shell command."},
+    {"Point", py_Point, METH_VARARGS, "Point Struct"},
+    {"distance", py_distance, METH_VARARGS, "distance between two points"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
@@ -22,32 +66,4 @@ PyMODINIT_FUNC
 initspam(void)
 {
     (void) Py_InitModule("spam", SpamMethods);
-}
-
-
-int
-main(int argc, char **argv)
-{
-    /* Pass argv[0] to the Python interpreter */
-    Py_SetProgramName(argv[0]);
-
-    /* Initialize the Python interpreter.  Required. */
-    Py_Initialize();
-
-    /* Add a static module */
-    initspam();
-
-    /* Do some application specific code */
-    printf("Hello, brave new world\n\n");
-
-
-    /* Note that you can call any public function of the Python
-       interpreter here, e.g. call_object(). */
-
-    /* Some more application specific code */
-    printf("\nGoodbye, cruel world\n");
-
-    /* Exit, cleaning up the interpreter */
-    Py_Exit(0);
-    /*NOTREACHED*/
 }
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import spam
 
-status = spam.system("ls -l")
+p1 = spam.Point(2, 3)
+p2 = spam.Point(4, 5)
+
+print p1
+print p2
+
+print spam.distance(p1, p2)
